5145a8f69b68985afd6c75a1f1bab2ed45b41e2c
Amy Thompson <amythompson9@gmail.com>
2018-09-18 11:30:38 -0400

    Exercise 1

--- IntrojQueryUdemy/JavaScript.js
+++ IntrojQueryUdemy/JavaScript.js
@@ -1,1 +1,7 @@
-﻿
+﻿//Select all divs and give them a purple background
+//Select the divs with class "highlight" and make them 200 px wide
+//Select the div with id "third" and give it an orange border
+//Bonus: Select the first div only and change its font color to pink-- >$("div").css("background", "purple");
+$(".highlight").css("width", "200px");
+$("#third").css("border", "2px solid orange");
+$("div:first-of-type").css("color", "pink");
